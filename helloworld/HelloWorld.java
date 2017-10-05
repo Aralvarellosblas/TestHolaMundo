543c3585ef124eb0ea08cf94b229846600f01d29
Arturo <Arturo@Arturo>
2017-10-05 09:17:33 +0200

    Comentarios añadidos

--- helloworld/HelloWorld.java
+++ helloworld/HelloWorld.java
@@ -4,8 +4,9 @@ public class HelloWorld {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello World");
-        System.out.println("Test for Github");
+        System.out.println("Hello World");  //Muestra el texto Hello World
+        System.out.println("Test for Github");  //Muestra el texto Test fot Github
+        
     }
     
 }
